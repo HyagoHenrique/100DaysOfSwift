39602f6755eb92058f1110056109486ac3be66fb
Hyago Henrique <hyagoh@icloud.com>
2021-11-23 21:39:38 -0300

    almost done

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -12,9 +12,25 @@ class ViewController: UIViewController {
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
     
+    var countries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countries += ["us","uk","estonia","frace", "germany", "ireland", "italy", "monaco", "russia", "poland", "spain", "nigeria"]
+        
+        view.backgroundColor = UIColor.lightGray
+        
+        askQuestion()
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
     }
 
 
