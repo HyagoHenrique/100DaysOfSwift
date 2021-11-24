4c906fc3d280dc28728c9eeeed416640c6bb1490
Hyago Henrique <hyagoh@icloud.com>
2021-11-23 21:03:14 -0300

    project2

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Hyago Henrique on 23/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
